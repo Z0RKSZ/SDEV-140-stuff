6e9bacd9019cf1b312a400488c2bb4b48c5c5f7a
Zorksz <oskarabeefv2@gmail.com>
2026-01-22 18:52:35 +0000

    checking work

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("Hello World")
 number = 30
 print(number)
+numbers=[10,20,30,40,50]
+numbers.append(60)
+numbers[2]=35
+numbers.pop(0)
+numbers.remove(40)
+print(numbers)
+nums={1,2,3,4}
+nums.add(5)
+nums.remove(2)
+nums.add(3)
+print(nums)
